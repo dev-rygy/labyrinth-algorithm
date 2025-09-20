8127e1d8216fae40bd56936802fd4d3fe982ca22
Ryan Carpenter <dev.ryan1184@gmail.com>
2025-09-20 11:24:21 -0700

    Player State Machine Error Fix

--- Assets/Scripts/Entities/Player/PlayerStateMachine.cs
+++ Assets/Scripts/Entities/Player/PlayerStateMachine.cs
@@ -14,7 +14,6 @@ using RyansLibrary.Input;
 using RyansLibrary.Abilities;
 using UnityEngine;
 using System;
-using UnityEngine.InputSystem;
 
 public enum PlayerStates
 {
@@ -120,10 +119,14 @@ public class PlayerStateMachine : StateMachine
     private void Awake()
     {
         // Handle singleton; if instance has a reference and the reference is not this object
-        if (Instance != null && Instance != this)
+        if (Instance != null)
+        {
+            Debug.LogWarning("Player State Machine Warning: Another instance of PlayerStateMachine already exists. Deleting Object...");
             Destroy(gameObject);
-        else
-            Instance = this;
+            return;
+        }
+        
+        Instance = this;
 
         // Assign references on object
         Controller = GetComponent<CharacterController>();           // The player must have a character controller
@@ -153,11 +156,12 @@ public class PlayerStateMachine : StateMachine
         // Transition to the first state
         TransitionStates(PlayerStates.Idle);
 
+        // DISABLED FOR DEMO
         // Equip Primary and Secondary Weapons off rip if assigned in the inspector
-        EquipPrimaryWeapon(PrimaryWeapon);
-        EquipSecondaryWeapon(SecondaryWeapon);
+        //EquipPrimaryWeapon(PrimaryWeapon);
+        //EquipSecondaryWeapon(SecondaryWeapon);
 
-        SetAbility(DashAbility);
+        //SetAbility(DashAbility);
     }
 
     private void OnEnable()
